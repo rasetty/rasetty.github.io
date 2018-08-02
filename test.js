7ed033631ecfb351f1cb3cd028e3362ebc73189f
rasetty <41181573+rasetty@users.noreply.github.com>
2018-08-02 23:54:50 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,3 @@
-var str,
-    text = '';
-function getValue(){
-	str = document.getElementById('str').value;
-};
-
-
-function decod(){
-
-    for (var i = str.length - 1; i !== - 1; i--) {
-    	text += str[i];
-    };
-    document.getElementById('out').innerHTML = text;
-    text = '';
-};
 
 var message,
     token,
