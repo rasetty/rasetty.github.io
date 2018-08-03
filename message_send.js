fed43a247fad27941ee4d151f94551d2d5ab9be1
rasetty <41181573+rasetty@users.noreply.github.com>
2018-08-03 23:33:00 +0700

    Update message_send.js

--- message_send.js
+++ message_send.js
@@ -9,7 +9,7 @@ function start(){
     count = document.getElementById('count').value;
 	begin = document.getElementById('begin').value - 1;
 	interval = document.getElementById('interval').value * 1000;
-	token = document.getElementById('token-in').value;
+	token = document.getElementById('token').value;
 	link = document.getElementById('link').value;
     if (message == ''){
     	alert('Не введен текст рассылки');
@@ -34,13 +34,13 @@ function start(){
     alert('Рассылка началась, будет закончена через ' + (Math.round(count * interval / 60 / 1000)) + ' минут(ы)');
     interval + 0;
     function func(){ 
-        $.post('https://api.vk.com/method/messages.send', { 
+        $.ajax('https://api.vk.com/method/messages.send', { 
         access_token: token, 
         v: '5.80', 
         peer_id: 2000000000 + begin, 
         message: message, 
         attachment: link
-        }, console.log), 
+        }), 
         begin++ 
     }; 
     var timerId = setInterval(func, interval); 
@@ -48,3 +48,44 @@ function start(){
     clearInterval(timerId); 
     }, interval * count);
 };
+
+
+function getCookie(name) {
+    var matches = document.cookie.match(new RegExp(
+    '(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+  $(document).ready(function () {
+    $('#token').val(getCookie('tokenSave'));
+  });
+  $('#token').keyup(function (eventObject) {
+    document.cookie = 'tokenSave=' + eventObject.target.value;
+  });
+  $(document).ready(function () {
+    $('#text').val(getCookie('tokenSave'));
+  });
+  $('#text').keyup(function (eventObject) {
+    document.cookie = 'tokenSave=' + eventObject.target.value;
+  });$(document).ready(function () {
+    $('#interval').val(getCookie('tokenSave'));
+  });
+  $('#interval').keyup(function (eventObject) {
+    document.cookie = 'tokenSave=' + eventObject.target.value;
+  });$(document).ready(function () {
+    $('#begin').val(getCookie('tokenSave'));
+  });
+  $('#begin').keyup(function (eventObject) {
+    document.cookie = 'tokenSave=' + eventObject.target.value;
+  });
+  $(document).ready(function () {
+    $('#link').val(getCookie('tokenSave'));
+  });
+  $('#link').keyup(function (eventObject) {
+    document.cookie = 'tokenSave=' + eventObject.target.value;
+  });
+  $(document).ready(function () {
+    $('#count').val(getCookie('tokenSave'));
+  });
+  $('#count').keyup(function (eventObject) {
+    document.cookie = 'tokenSave=' + eventObject.target.value;
+  });
