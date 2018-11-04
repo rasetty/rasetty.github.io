807e72aacb73c20ac77e13148b00a3b12c393893
rasetty <41181573+rasetty@users.noreply.github.com>
2018-11-04 11:22:44 +0700

    Add files via upload

---
+++ table.js
@@ -0,0 +1,50 @@
+class Order {
+  constructor(code = '', label = '', price = '', amount = '') {
+    this.code = code;
+    this.label = label;
+    this.price = price;
+    this.amount = amount;
+  }
+}
+
+new Vue ({
+  el: '.container',
+  
+  data: {
+    orders: [new Order('', '', '', '')],
+    check: ''
+  },
+  
+  computed: {
+    result() {
+      let toReturn = 0;
+      
+      for (let order of this.orders)
+         toReturn += order.price * order.amount;
+      
+      return toReturn;
+    },
+
+    result2() {
+      let toReturn = 0;
+      
+      for (let order of this.orders)
+         toReturn += order.price * order.amount * 0.15;
+      
+      return toReturn;
+    },
+    password() {
+      let toReturn = true
+      if (this.check == 'ghbdtn') 
+        return toReturn
+      
+    }
+    
+  },
+  
+  methods: {
+    addNewOrder() {
+      this.orders.push(new Order);
+    }
+  }
+});
