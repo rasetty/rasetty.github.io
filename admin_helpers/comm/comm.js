be61de0a48c41ed53a01fad45fe3e334fec7d111
rasetty <41181573+rasetty@users.noreply.github.com>
2020-05-02 17:15:46 +0700

    Add files via upload

---
+++ admin_helpers/comm/comm.js
@@ -0,0 +1,91 @@
+//создание новых вложений
+var iForName = 0;
+function create(){
+	if (iForName >= 10){
+		return
+	}else{
+		iForName++;
+	};
+	var name = 'link' + iForName;
+	var em = document.createElement('em');
+	em.innerHTML = 'Ссылка на прикрепляемый файл';
+	var input = document.createElement('input');
+	input.setAttribute('type', 'text');
+	input.setAttribute('class', 'input');
+	input.setAttribute('id', name);
+	var place = document.getElementById('js');
+	place.appendChild(em);
+	place.appendChild(input);
+}
+//основная функция
+function start() {
+//создание переменных для работы
+	let comm = document.getElementById('comm').value.split('\n'),
+		interval = document.getElementById('interval').value * 1000,
+		attachments,
+		data,
+		token = document.getElementById('token').value;
+	for (let i = 0; i <= iForName; i++) {
+		attachments = document.getElementById('link' + i).value + ',';
+	}
+	setInterval(()=>{
+//создание рандомов
+		var randComm = Math.floor(Math.random() * comm.length),
+			randAtt = Math.floor(Math.random() * attachments.length);
+//аякс запрос
+		$.ajax({
+			url: 'https://api.vk.com/method/newsfeed.get',
+			jsonp: 'callback',
+			dataType: 'jsonp',
+			data: {
+				access_token: token,
+				filters: "post",
+				count: 1,
+				v: '5.103'
+			},
+		
+			success: jsonp=>{ //саксесс
+				let firstPost = jsonp["response"]["items"][0], //первый пост полученный
+					time = firstPost["date"], //время поста
+					group = jsonp["response"]["groups"][0];//первая группа полученаая
+				if (!data || time === data) data = time //проверка данных на существование
+				if (time !== data) {
+					data = 0;
+					if (!firstPost["source_id"]) return //если поста не существует
+					$.ajax({
+					url: 'https://api.vk.com/method/wall.createComment',
+					jsonp: 'callback',
+					dataType: 'jsonp',
+					data: {
+						access_token: token,
+						owner_id: firstPost["source_id"],
+						post_id: firstPost["post_id"],
+						attachments: !attachments ? "" : attachments[randAtt],
+						message: comm[randComm],
+						v: '5.103'
+					}, success: ()=>{document.getElementById('log').innerHTML += "\nНовый комментарий: https://vk.com/wall" + firstPost["source_id"] + "_" + firstPost["post_id"] + " в группе: " + group["name"]}})
+				}
+			}
+		})
+	}, interval);
+}
+//удаление данных
+function clear(){
+	localStorage.clear();
+	window.location.reload();
+}
+//сохранение данных
+function save(){
+	localStorage.setItem('tokenS', document.getElementById('token').value); 
+	localStorage.setItem('commS', document.getElementById('comm').value); 
+	localStorage.setItem('intervalS', document.getElementById('interval').value); 
+}
+//восстановление сохраненных данных
+document.getElementById('token').value = localStorage.getItem('tokenS');
+document.getElementById('comm').value = localStorage.getItem('commS');
+document.getElementById('interval').value = localStorage.getItem('intervalS');
+//события документа
+document.getElementById('create').onclick = create;
+document.getElementById('start').onclick = start;
+document.getElementById('saveData').onclick = save;
+document.getElementById('clearData').onclick = clear;
