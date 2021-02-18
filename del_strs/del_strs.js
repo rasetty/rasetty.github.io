db321500bae04f8149bb50196146e412c20960ef
rasetty <41181573+rasetty@users.noreply.github.com>
2021-02-18 21:31:48 +0700

    Add files via upload

--- del_strs/del_strs.js
+++ del_strs/del_strs.js
@@ -1,13 +1,16 @@
+//переменные из DOM
 const orig = document.getElementById('orig'),
 	del = document.getElementById('del'),
 	amountOrig = document.getElementById('amountOrig'),
 	amountDel = document.getElementById('amountDel');
+//назначение кнопок
 orig.addEventListener('keyup', (event)=>{
 	if(event.keyCode == 13 && event.ctrlKey) format();
 })
 del.addEventListener('keyup', (event)=>{
 	if(event.keyCode == 13 && event.ctrlKey) format();
 })
+//удаление пустых строк при вставке
 orig.oninput = ()=>{
 	let textOrig = orig.value,
 		counter = 0;
@@ -25,8 +28,8 @@ del.oninput = ()=>{
 	let textDel = del.value,
 		counter = 0;
 
-	for(let i = 0; i < textDel.length; i++) {
-		if (textDel[i] == '\n') counter++;
+	for(let i = 0; i < textDel.length; i++){
+		if (textDel[i] == '\n')counter++;
 	}
 	for(let i = counter; i > 0; i--){
 		textDel = textDel.replaceAll('\n\n', '\n');
@@ -35,13 +38,33 @@ del.oninput = ()=>{
 	amountDel.innerHTML = 'Strings: ' + del.value.split('\n').length;
 }
 
-
+//функция удаления
 function format(){
-	let textOrig = orig.value;
+	//массивы строк
+	let textOrig = orig.value.split('\n');
 	const textDel = del.value.split('\n');
-	for (let i = 0; i <= textDel.length; i++){
-		textOrig = textOrig.replaceAll('\n' + textDel[i], '');
-		textOrig = textOrig.replaceAll(textDel[i] + '\n', '');
+	//сравнение каждой строки с каждой и делит
+	for(let j = 0; j < textDel.length; j++){
+		for (let i = textOrig.length; i > 0; i--){
+			if (textOrig[i] == textDel[j]){
+				textOrig.splice(i, 1)
+			}
+		}
 	}
-	orig.value = textOrig;
+	//удаление пустых
+	for(let i = 0; i < textDel.length; i++){
+		if(textDel[i] === ''){
+			textDel.splice(i, 1);
+		}
+	}
+	for(let i = 0; i < textOrig.length; i++){
+		if(textOrig[i] === ''){
+			textOrig.splice(i, 1);
+		}
+	}
+	//обнуление и вывод
+	orig.value = '';
+	for (let i = 0; i < textOrig.length; i++){
+		orig.value += textOrig[i] + '\n';
+	}
 }
